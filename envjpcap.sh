4a903a79a581cf3827aa29fa69c6be201016fa23
root <root@debian-BULLSEYE-live-builder-AMD64>
2023-06-24 06:38:46 +0000

    ok

--- envjpcap.sh
+++ envjpcap.sh
@@ -1,2 +1,3 @@
+export LIB_PCAP=/usr/lib/x86_64-linux-gnu/
 export LD_LIBRARY_PATH=~/jpcap/src/java/net/sourceforge/jpcap/capture/
 export CLASSPATH=~/jpcap/src/java/:~/jpcap/src/java:~/jpcap/thirdParty/jars/fooware_CommandLine-1.0.jar:~/jpcap/thirdParty/jars/dev-classes_net.ultrametrics-0.03.jar:~/jpcap/thirdParty/jars/junit.jar
